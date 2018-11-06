0edc43b17a917864ac9bac8cbf48b6b83339c9cb
cppswengr <cppswengr@eVantageGroup.com>
2018-11-06 03:57:24 -0600

    In ch2, upto but not including dicts and sets

--- ch2/player.py
+++ ch2/player.py
@@ -1,3 +1,5 @@
+import textwrap
+
 class player(object):
 
     default_player_class = 'warrior'
@@ -10,6 +12,34 @@ class player(object):
                       'sorcerer': 'sorcerer',
                       'undead':   'undead',
                        }
+
+    player_short_class_descriptions = {
+        'warrior':
+            'A battle-hardened veteran; can equip heavy armor and weapons.',
+         'sorcerer':
+            'sorcerer',
+         'undead':
+             'undead',
+    }
+
+    player_long_class_descriptions = {
+        'warrior':
+           '''\
+           A seasoned veteran of many battles.  High Strength and Dexterity
+           allow him or her to yield heavy armor and weapons, as well as carry
+           more equipment while keeping a light roll.  Weak in magic
+           ''',
+        'sorcerer':
+            'sorcerer',
+        'undead':
+            'undead',
+    }
+
+    player_starting_equipment = {
+        'warrior': ['long sword', 'warrior set', 'shield'],
+        'sorcerer': ['wand'],
+        'undead': [],
+    }
 
     def __init__(self, player_class):
         self.health = self.set_default_health( player_class )
@@ -26,6 +56,20 @@ class player(object):
 
     def get_default_player_class(self):
         return self.default_player_class
+
+    def get_player_class(self):
+        return self.player_class
+
+    def get_short_class_description(self):
+        return self.player_short_class_descriptions[ self.get_player_class() ]
+
+    def get_long_class_description(self):
+        return textwrap.dedent(
+            self.player_long_class_descriptions[ self.get_player_class() ]
+        )
+
+    def get_player_starting_equipment(self):
+        return self.player_starting_equipment[ self.get_player_class() ]
 
     def hit( self, player ):
         player.get_hit()
--- ch2/test_player_mechanics.py
+++ ch2/test_player_mechanics.py
@@ -1,4 +1,6 @@
+import textwrap
 from .player import player
+
 
 def test_player_hit():
     kartoon = player('warrior')
@@ -18,3 +20,30 @@ def test_default_player_class():
     munch = player('undead')
     x = munch.get_default_player_class()
     assert x == 'warrior'
+
+
+def test_get_player_class():
+    munch = player("undead")
+    x = munch.get_player_class()
+    assert x == "undead"
+
+
+def test_warrior_short_description():
+    kartoon = player('warrior')
+    desc = kartoon.get_short_class_description()
+    assert desc == "A battle-hardened veteran; can equip heavy armor and weapons."
+
+
+def test_warrior_long_description():
+    kartoon = player('warrior')
+    desc = kartoon.get_long_class_description()
+    assert desc == textwrap.dedent('''\
+        A seasoned veteran of many battles.  High Strength and Dexterity
+        allow him or her to yield heavy armor and weapons, as well as carry
+        more equipment while keeping a light roll.  Weak in magic
+        ''')
+
+def test_get_warrior_starting_equipment():
+    kartoon = player('warrior')
+    expected = ['long sword', 'warrior set', 'shield']
+    assert kartoon.get_player_starting_equipment() == expected
